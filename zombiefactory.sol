5a3af6fb53a934db235fc1a0fa7db6402d04f121
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-06-02 10:16:12 -0300

    Update struct
    Add win and loss count

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -15,6 +15,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
     Zombie[] public zombies;
 
@@ -26,7 +28,7 @@ contract ZombieFactory is Ownable {
     /// @oaram _dna a uint with the zombie's dna
     /// @dev the event NewZombie is fired here
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
