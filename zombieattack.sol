b291596a5706eaa9fb2c3f45a0c00fb77e235a27
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2018-06-05 09:57:47 -0300

    Update
    Update with SafeMath functions

--- zombieattack.sol
+++ zombieattack.sol
@@ -9,7 +9,7 @@ contract ZombieBattle is ZombieHelper {
     /// @param _modulus Defines the number of digits the random number will have
     /// @return the random number 
     function randMod(uint _modulus) internal returns(uint) {
-        randNonce++;
+        randNonce = randNonce.add(1);
         return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
     }
 
@@ -21,13 +21,13 @@ contract ZombieBattle is ZombieHelper {
         Zombie storage enemyZombie = zombies[_targetId];
         uint rand = randMod(100);
         if (rand <= attackVictoryProbability) {
-            myZombie.winCount++;
-            myZombie.level++;
-            enemyZombie.lossCount++;
+            myZombie.winCount = myZombie.winCount.add(1);
+            myZombie.level = myZombie.level.add(1);
+            enemyZombie.lossCount = enemyZombie.lossCount.add(1);
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
         } else {
-            myZombie.lossCount++;
-            enemyZombie.winCount++;
+            myZombie.lossCount = myZombie.lossCount.add(1);
+            enemyZombie.winCount = enemyZombie.winCount.add(1);
             _triggerCooldown(myZombie);
         }
     }
